d1bbcf6aa1cbe73585da2f6d4d5ed88d128c0a59
Calleodavid <50590814+Calleodavid@users.noreply.github.com>
2019-05-13 13:54:05 -0300

    Jogo de Dados

---
+++ Lógica de Programação/Calleo.py
@@ -0,0 +1,15 @@
+from random import randint # Importa a função de gerar números aleatórios 
+
+print("Olá 1info02!")
+
+Dado1 = randint(1, 6)
+print("dado1: ", dado1)
+Dado2 = randint(1, 6)
+print("dado2: ", dado2)
+Dado3 = randint(1, 6)
+print("dado3: ", dado3)
+Dado4 = randint(1, 6)
+print("dado4: ", dado4)
+
+jogador1 = Dado1 + Dado2
+jogador2 = Dado3 + Dado4
